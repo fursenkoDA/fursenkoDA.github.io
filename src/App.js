12cce451fb9ba16b7a79a0c6c1dd70661952069d
RobuASD <Roollexx@live.com>
2021-08-27 22:12:00 +1000

    #Add, create sourse template

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import Avatar from 'react-avatar'
+import av from './icons/rbcrf.jpg'
+import tlg from './icons/images.png'
+import inst from './icons/inst.jpg'
+import serf from './icons/serf.png'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <nav className="navbar">
+        <h1>Дарья Фурсенко</h1>
+        <div className="links">
+          <a href="https://t.me/akali999">
+            <Avatar src={tlg} size="35" round={true} />
+          </a>
+          <a href="https://www.instagram.com/f.daria.al/">
+            <Avatar src={inst} size="35" round={true} />
+          </a>
+        </div>
+      </nav>
+      <div className="content">
+        <div className="bio-details">
+          <article className="avatar">
+            <Avatar src={av} round={true} />
+          </article>
+          <article>
+            <h2>Основыне данные</h2>
+            <p>Город проживания:</p>
+            <p>Владивосток (UTC +10)</p>
+            <p>Телефон:</p>
+            <p>+7-914-711-45-77</p>
+            <p>Email:</p>
+            <p>fursenkodd99@icloud.com</p>
+          </article>
+        </div>
+        <div className="blog-preview">
+          <article>
+            <h2>Навыки:</h2>
+            <p>Закупка рекламы</p>
+            <p>Продажа рекламы</p>
+            <p>Разбор директа</p>
+            <p>Создание и ведение PR-аккаунта</p>
+          </article>
+        </div>
+        <div className="blog-preview">
+          <article>
+            <img className="serfImg" src={serf} />
+          </article>
+        </div>
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
